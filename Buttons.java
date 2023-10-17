29f8b996e301608bfdbc45ffedcf27d472613468
Smaash7 <goncalo.cr62@gmail.com>
2023-10-17 17:04:03 +0100

    InitialMenu

---
+++ Buttons.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Buttons here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Buttons extends Actor
+{
+    private boolean mouseEmCima = false;
+    private static int MAX_TRANS = 225;
+    
+    public void checkMouse()
+    {
+        if(Greenfoot.mouseMoved(null))
+        {
+            mouseEmCima = Greenfoot.mouseMoved(this);
+        }
+        if(mouseEmCima)
+        {
+            adjTrans(MAX_TRANS/2);
+        }
+        else
+        {
+        adjTrans(MAX_TRANS);
+        }
+    }
+    public void checkClick(World world)
+    { 
+        if(Greenfoot.mouseClicked(this))
+        {
+            Greenfoot.setWorld(world);
+        }
+    
+    }
+    public void adjTrans(int adjust)
+    {
+        GreenfootImage tempImage = getImage();
+        tempImage.setTransparency(adjust);
+        setImage(tempImage);
+    }
+}
